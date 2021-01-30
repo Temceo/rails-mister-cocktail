88b7618b87b65f85d7092a7330545c13dd73b282
Temceo <martinmwaka@hotmail.com>
2021-01-30 16:27:47 +0000

    completed cocktails / doses views

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,5 +1,5 @@
 class DosesController < ApplicationController
-  before_action :find_cocktail 
+  before_action :find_cocktail, except: [ :destroy ]
   
   def new 
     @dose = Dose.new
@@ -13,6 +13,12 @@ class DosesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
