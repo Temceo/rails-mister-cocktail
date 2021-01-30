109134398eaaaeb9413b501a2d1f5b0f86259960
Temceo <martinmwaka@hotmail.com>
2021-01-30 14:55:17 +0000

    added dose and ingredient models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :new, :create, :show, :index ]
+  resources :cocktails, only: [ :new, :create, :show, :index ] do 
+    resources :doses, only: [ :new, :create, :destroy ]
+  end
 end
